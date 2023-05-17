a669f85ed46af8d5c5695c2cbc548e01d7d8fe4b
egy351 <129901135+egy351@users.noreply.github.com>
2023-05-17 12:38:49 +0200

    dom elements created with js

--- js/script.js
+++ js/script.js
@@ -1,56 +1,67 @@
-// targeting elements from the DOM
-const numberZero = document.querySelector('#number')
-const minusButton = document.querySelector('#minus')
-const plusButton = document.querySelector('#plus')
+// creating main elements in the DOM
+const counterContainer = document.querySelector('#counter');
+const buttonsContainer = document.createElement('div');
+
+const numberZero = document.createElement('div');
+const minusButton = document.createElement('button');
+const plusButton = document.createElement('button');
+
+// nesting number display and creating relative id
+counterContainer.appendChild(numberZero);
+numberZero.setAttribute('id', 'number');
+
+// nesting buttons and creating relative ids
+counterContainer.appendChild(buttonsContainer);
+buttonsContainer.setAttribute('id', 'buttons');
+
+buttonsContainer.appendChild(minusButton);
+minusButton.setAttribute('id', 'minus');
+
+buttonsContainer.appendChild(plusButton);
+plusButton.setAttribute('id', 'plus');
 
 // writing objects on the document
 let numbers = [
   {
-    starting: 0
+    starting: 0,
   },
 ];
 
-let zero = numbers.map(function(item) {
-  return (
-      item.starting
-    );
-})
+let zero = numbers.map(function (item) {
+  return item.starting;
+});
 
 numberZero.innerHTML = zero.join('\n');
 
 let buttons = [
   {
     plus: '+',
-    minus: '-'
+    minus: '-',
   },
 ];
 
-let minor = buttons.map(function(item) {
-  return (
-    item.minus
-  )
-})
+let minor = buttons.map(function (item) {
+  return item.minus;
+});
 
-let major = buttons.map(function(item) {
-  return (
-    item.plus
-  );
-})
+let major = buttons.map(function (item) {
+  return item.plus;
+});
 
 minusButton.innerHTML = minor.join('\n');
 plusButton.innerHTML = major.join('\n');
 
-// counter main functions
+// developing counter main functions
 var counter = 0;
 
-plusButton.onclick = function add(){
+plusButton.onclick = function add() {
   counter++;
-  document.querySelector('#number').innerHTML = counter
-}
+  document.querySelector('#number').innerHTML = counter;
+};
 
-minusButton.onclick = function subtract(){
+minusButton.onclick = function subtract() {
   if (counter != 0) {
     counter--;
   }
-  document.querySelector('#number').innerHTML = counter
-}
+  document.querySelector('#number').innerHTML = counter;
+};
