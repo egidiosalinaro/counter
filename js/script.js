d76739e96eb32efa7c6ba7ae51c67a948f1713e5
egy351 <129901135+egy351@users.noreply.github.com>
2023-05-27 18:50:11 +0200

    code cleaning

--- js/script.js
+++ js/script.js
@@ -1,69 +1,41 @@
-// creating main elements in the DOM
-const counterContainer = document.querySelector('#counter');
-const buttonsContainer = document.createElement('div');
+// creating and naming elements in the counter
+const counterBox = document.querySelector('#counter');
 
-const numberZero = document.createElement('div');
-const minusButton = document.createElement('button');
-const plusButton = document.createElement('button');
+function createCounterElement(tagHtml, attribute, content) {
+  const newElement = document.createElement(tagHtml);
+  newElement.setAttribute('id', attribute);
+  newElement.innerHTML = content;
+  counterBox.appendChild(newElement);
+}
 
-// nesting number display and creating relative id
-counterContainer.appendChild(numberZero);
-numberZero.setAttribute('id', 'number');
+createCounterElement('div', 'number', 0);
+createCounterElement('div', 'buttons', '');
+const numberZero = document.querySelector('#number');
+const buttonsBox = document.querySelector('#buttons');
 
-// nesting buttons and creating relative ids
-counterContainer.appendChild(buttonsContainer);
-buttonsContainer.setAttribute('id', 'buttons');
+// creating and naming elements in the buttons section
+function createButtonsElement(tagHtml, attribute, content) {
+  const newElement = document.createElement(tagHtml);
+  newElement.setAttribute('id', attribute);
+  newElement.innerHTML = content;
+  buttonsBox.appendChild(newElement);
+}
 
-buttonsContainer.appendChild(minusButton);
-minusButton.setAttribute('id', 'minus');
-
-buttonsContainer.appendChild(plusButton);
-plusButton.setAttribute('id', 'plus');
-
-// writing objects in the document
-let numbers = [
-  {
-    starting: 0,
-  },
-];
-
-let zero = numbers.map(function (item) {
-  return item.starting;
-});
-
-numberZero.innerHTML = zero.join('\n');
-
-let buttons = [
-  {
-    plus: '+',
-    minus: '-',
-  },
-];
-
-let minor = buttons.map(function (item) {
-  return item.minus;
-});
-
-let major = buttons.map(function (item) {
-  return item.plus;
-});
-
-minusButton.innerHTML = minor.join('\n');
-plusButton.innerHTML = major.join('\n');
+createButtonsElement('button', 'minus', '-');
+createButtonsElement('button', 'plus', '+');
+const minusButton = document.querySelector('#minus');
+const plusButton = document.querySelector('#plus');
 
 // developing counter main functions
-var counter = 0;
-
-buttonsContainer.addEventListener('click', event => {
+let counter = 0;
+buttonsBox.addEventListener('click', event => {
   const target = event.target;
 
   if (target === plusButton) {
     counter++;
   }
-
   if (target === minusButton && counter != 0) {
     counter--;
   }
-
   numberZero.innerHTML = counter;
 });
